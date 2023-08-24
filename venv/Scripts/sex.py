475b3a76fdffa0470ead6f0b13c0082ab8736a1b
Saz <jesrisak@gmail.com>
2023-08-24 20:43:48 +0200

    adding words

--- venv/Scripts/sex.py
+++ venv/Scripts/sex.py
@@ -1,11 +1,18 @@
+import random
 # tjeeeeena
 # TODO: sdsd
 
-sex
-print("välkommen till häng gubben")
-ord = ["abc", "defghi", "jkl"]
-i = 0
 
-while i < len(ord[1]):
+
+trys = 10
+
+ord = ["abc", "defghi", "jklm"]
+ran = random.randrange(0,len(ord))
+txt = "välkommen till hänga gubben, ordet har {} bokstäver"
+
+print(txt.format(len(ord[ran])))
+i = 0
+while i < len(ord[ran]):
     print("___ ", end="")
     i += 1
+
