6b8121d86ae7cc72bf5720aaac8f348f318ae916
Saz <jesrisak@gmail.com>
2023-09-04 09:37:46 +0200

    ord checkas faktiskt :) men inte går med flera rätta gissningar :(

--- venv/Scripts/sex.py
+++ venv/Scripts/sex.py
@@ -1,30 +1,56 @@
 import random
+
 # tjeeeeena
 # TODO: sdsd
 
 
-
 trys = 10
+ans = 0
 
-words = ["abc", "defghi", "jklm"]
-ran = random.randrange(0,len(words))
+words = ["aaaf", "defghh", "jkkml"]
+ran = random.randrange(0, len(words))
 txt = "välkommen till hänga gubben, ordet har {} bokstäver"
 vald = words[ran]
 alpha = list(vald)
+svar = [None] * len(vald)
 
 print(txt.format(len(vald)))
-i = 0
-while i < len(vald):
-    print("___ ", end="")
-    i += 1
 
-    
-while trys>1:
+
+def sex(guess):
+    for i in range(len(vald)):
+        if guess not in alpha[i] or guess == str() or svar[i] is None:
+            svar[i] = "___"
+            i += 1
+
+        else:
+            svar[i] = guess
+            i += 1
+
+    print(svar)
+
+
+while trys > 1 or ans != len(vald):
+
     print(trys, " ", end="\n")
+
     guess = input("gissning: ")
-    if guess.isdigit() or 1!=len(guess):
-        print(vald, alpha)
+
+    print(vald, alpha)
+
+    if guess.isdigit() or 1 != len(guess):
+        print("snälla använd endast 1 bokstav och inga siffror")
+
     elif guess in alpha:
-        continue
+        ans += 1
+        sex(guess)
+
     else:
-        trys-=1
+        trys -= 1
+
+
+'''
+while pouyan == gay in bed[]
+    pouyan += dick
+    pouyan -= dick
+'''
